96b5cd056d8ddbe08e25127bafcfd168181b9aff
Ben Stuart <ben@benstuart.ie>
2022-06-04 16:03:56 +0100

    Update setup script

--- setup.sh
+++ setup.sh
@@ -2,10 +2,16 @@
 
 WORK_DIR=/tmp/setup
 
-# Install pre-requisites
-dnf install -y python3 python3-pip git
-pip3 install ansible ansible-core
+read -s -p "Enter sudo password for $USER: " $password
+echo ""
 
-git clone https://github.com/benchoncy/my-workstation-setup-playbook.git $WORK_DIR
+echo "Installing pre-requisites..."
+echo $password | sudo -S -k dnf install -yq python3 python3-pip git 2>/dev/null
+pip -q install ansible ansible-core
 
-ansible-playbook --connection=local --inventory 127.0.0.1, --ask-become-pass $WORK_DIR/main.yml
+echo "Cloning repository..."
+rm -rf $WORK_DIR && \
+git clone -q https://github.com/benchoncy/my-workstation-setup-playbook.git $WORK_DIR
+
+echo "Running Ansible playbook..."
+ansible-playbook -c local -i 127.0.0.1, -e "ansible_become_pass=$password" $WORK_DIR/main.yml
